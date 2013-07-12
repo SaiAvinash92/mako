fbe260cb0f48896890e2bc4ff767befcab7d4629
Paul Reioux <reioux@gmail.com>
2013-07-12 01:20:16 -0500

    intelli/msm_thermal: initial coding for Qualcomm S4 Pro devices
    an intelligent MSM thermal throttling kernel driver designed to replace the
    closed sourced thermald(aemon) from Qualcomm.
    
    The current driver can throttle the SOC via core frequency scaling or via
    core hotplug with user configurable sysfs entries to adjust the throttling
    temperature thresholds for each.
    
    Derived from Qualcomm's Open source repo.
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    Conflicts:
    	arch/arm/mach-msm/lge/mako/board-mako.c

--- arch/arm/mach-msm/lge/mako/board-mako.c
+++ arch/arm/mach-msm/lge/mako/board-mako.c
@@ -1371,6 +1371,16 @@ static struct msm_thermal_data msm_thermal_pdata = {
 	.limit_temp_degC = 60,
 	.temp_hysteresis_degC = 10,
 	.freq_step = 2,
+#ifdef CONFIG_INTELLI_THERMAL
+	.freq_control_mask = 0xf,
+#ifdef CONFIG_CPU_OVERCLOCK
+	.core_limit_temp_degC = 90,
+#else
+	.core_limit_temp_degC = 80,
+#endif
+	.core_temp_hysteresis_degC = 10,
+	.core_control_mask = 0xe,
+#endif
 };
 
 #define MSM_SHARED_RAM_PHYS 0x80000000
