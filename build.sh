8549f0864c403b1cc4c5237ce18582b4990d6c09
SaiAvinash92 <aviz1911@gmail.com>
2014-11-23 23:33:46 +0530

    Add build script

---
+++ build.sh
@@ -0,0 +1,36 @@
+if [ $# -gt 0 ]; then
+echo $1 > .version
+fi
+
+make avinash_defconfig 
+make -j6
+ 
+cp arch/arm/boot/zImage ../ramdisk_mako/
+ 
+cd ../ramdisk_mako/
+ 
+echo "making ramdisk"
+./mkbootfs boot.img-ramdisk | gzip > ramdisk.gz
+echo "making boot image"
+./mkbootimg --kernel zImage --cmdline 'console=ttyHSL0,115200,n8 androidboot.hardware=mako lpj=67677 user_debug=31' --base 0x80200000 --pagesize 2048 --ramdisk_offset 0x01600000 --ramdisk ramdisk.gz --output ../mako/boot.img
+ 
+rm -rf ramdisk.gz
+rm -rf zImage
+ 
+cd ../mako/
+ 
+zipfile="nash.Kernel.zip"
+echo "making zip file"
+cp boot.img zip/
+ 
+rm -rf ../ramdisk_mako/boot.img
+ 
+cd zip/
+rm -f *.zip
+zip -r $zipfile *
+rm -f /tmp/*.zip
+cp *.zip /tmp
+mv *.zip /home/avinash/Android/FlashZip
+
+rm -rf boot.img
+rm -rf ../boot.img
