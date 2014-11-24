c5882c96204c4431acf0c6b83f8ad591ea9f9852
SaiAvinash92 <aviz1911@gmail.com>
2014-11-24 10:52:57 +0530

    Build script changes for LZ4

--- build.sh
+++ build.sh
@@ -10,11 +10,11 @@ cp arch/arm/boot/zImage ../ramdisk_mako/
 cd ../ramdisk_mako/
  
 echo "making ramdisk"
-./mkbootfs boot.img-ramdisk | gzip > ramdisk.gz
+./mkbootfs boot.img-ramdisk | ./lz4demo -c stdin ramdisk.lz4
 echo "making boot image"
-./mkbootimg --kernel zImage --cmdline 'console=ttyHSL0,115200,n8 androidboot.hardware=mako lpj=67677 user_debug=31' --base 0x80200000 --pagesize 2048 --ramdisk_offset 0x01600000 --ramdisk ramdisk.gz --output ../mako/boot.img
+./mkbootimg --kernel zImage --cmdline 'console=ttyHSL0,115200,n8 androidboot.hardware=mako lpj=67677 user_debug=31' --base 0x80200000 --pagesize 2048 --ramdisk_offset 0x02300000 --ramdisk ramdisk.lz4 --output ../mako/boot.img
  
-rm -rf ramdisk.gz
+rm -rf ramdisk.lz4
 rm -rf zImage
  
 cd ../mako/
