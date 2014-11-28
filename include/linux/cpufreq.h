c92013f1f97ce0eac183a968b72c8c956c18ed4d
SaiAvinash92 <aviz1911@gmail.com>
2014-11-28 15:19:25 +0530

    Add hyper CPU governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -397,6 +397,9 @@ extern struct cpufreq_governor cpufreq_gov_smartmax;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIMM)
 extern struct cpufreq_governor cpufreq_gov_intellimm;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intellimm)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HYPER)
+extern struct cpufreq_governor cpufreq_gov_HYPER;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_HYPER)
 #endif
 
 
